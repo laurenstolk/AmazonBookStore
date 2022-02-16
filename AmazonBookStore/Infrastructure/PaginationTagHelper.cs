4b671d79dbfa6f21fac9cea6e67d6a5e269e5433
laurenstolk <laurenstolk@outlook.com>
2022-02-15 18:04:53 -0700

    bootstrap

--- AmazonBookStore/Infrastructure/PaginationTagHelper.cs
+++ AmazonBookStore/Infrastructure/PaginationTagHelper.cs
@@ -30,6 +30,11 @@ namespace AmazonBookStore.Infrastructure
 
         public string PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
 
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
         {
@@ -42,6 +47,13 @@ namespace AmazonBookStore.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
